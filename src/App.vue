9be323b7443f9317b59f57a33c3b0d6f630bdd56
ssabrut <michael.gunawan2002@gmail.com>
2024-03-02 13:03:29 +0700

    Add bubble ui

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import { onMounted, ref } from 'vue';
 
 import Navigation from './components/Navigation.vue';
 import Input from './components/Input.vue';
+import Bubble from './components/Bubble.vue';
 
 const pinecone = ref(null);
 const index = ref(null);
@@ -20,18 +21,7 @@ onMounted(async () => {
   </nav>
   <main class="container mx-auto">
     <div class="mb-8">
-      <div class="flex items-start gap-2.5">
-        <div
-          class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-blue-950 rounded-e-xl rounded-es-xl">
-          <div class="flex items-center space-x-2 rtl:space-x-reverse">
-            <span class="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
-            <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
-          </div>
-          <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will
-            really appreciate the improvements.</p>
-          <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
-        </div>
-      </div>
+      <Bubble />
     </div>
     <div>
       <Input />
---
+++ src/components/Bubble.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex items-start gap-2.5">
+        <img class="w-8 h-8 rounded-full" src="../assets/pixlr-image-generator-51abc63a-2494-4e5f-8bf8-e3e8704617cd.png"
+            alt="Jese image">
+        <div
+            class="flex flex-col w-full max-w-4xl leading-1.5 p-4 border-gray-200 bg-blue-950 rounded-e-xl rounded-es-xl">
+            <div class="flex items-center space-x-2 rtl:space-x-reverse">
+                <span class="text-sm font-semibold text-gray-900 dark:text-white">Analyzer</span>
+            </div>
+            <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will
+                really appreciate the improvements.</p>
+        </div>
+    </div>
+</template>
