549593b3d2b25e2884ee7a8470a8f6a979269df6
ssabrut <michael.gunawan2002@gmail.com>
2024-03-02 13:53:53 +0700

    Add user ui bubble

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@ import { Pinecone } from '@pinecone-database/pinecone';
 import { onMounted, ref } from 'vue';
 
 import Navigation from './components/Navigation.vue';
-import Input from './components/Input.vue';
 import Bubble from './components/Bubble.vue';
 
 const pinecone = ref(null);
 const index = ref(null);
 const histories = ref([]);
+const comment = ref('');
 
 onMounted(async () => {
   pinecone.value = new Pinecone({ apiKey: import.meta.env.VITE_PINECONE_API_KEY });
@@ -16,6 +16,10 @@ onMounted(async () => {
   histories.value.push({
     message: 'Hello, I am Airline Sentiment Bot. Start Writing Your Comments and I will analyze them for you.',
     isUser: false,
+  });
+  histories.value.push({
+    message: 'Hello, I am Airline Sentiment Bot. Start Writing Your Comments and I will analyze them for you.',
+    isUser: true,
   });
 })
 </script>
@@ -29,7 +33,20 @@ onMounted(async () => {
       <Bubble v-for="history in histories" :isUser="history.isUser" :message="history.message" />
     </div>
     <div>
-      <Input />
+      <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
+        <div class="px-4 py-2.5 bg-white rounded-t-lg">
+          <label for="comment" class="sr-only">Your comment</label>
+          <textarea id="comment" rows="4"
+            class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-transparent focus:border-transparent" placeholder="Write your comment here..."
+            v-model="comment" />
+        </div>
+        <div class="flex items-center justify-end px-3 py-2.5 border-t">
+          <button type="submit"
+            class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-950 rounded-lg focus:ring-4 focus:ring-blue-200">
+            Post comment
+          </button>
+        </div>
+      </div>
     </div>
   </main>
 </template>
--- src/components/Bubble.vue
+++ src/components/Bubble.vue
@@ -12,11 +12,11 @@ defineProps({
 </script>
 
 <template>
-    <div class="flex items-start gap-2.5 mb-4">
+    <div class="flex items-start gap-2.5 mb-4" :class="isUser ? ' flex-row-reverse' : ''">
         <img class="w-8 h-8 rounded-full" :src="isUser ? '/src/assets/user.png' :'/src/assets/pixlr-image-generator-51abc63a-2494-4e5f-8bf8-e3e8704617cd.png'"
             alt="Jese image">
         <div
-            class="flex flex-col w-full max-w-4xl leading-1.5 p-4 border-gray-200 bg-blue-950 rounded-e-xl rounded-es-xl">
+            class="flex flex-col w-full max-w-4xl leading-1.5 p-4 border-gray-200 bg-blue-950" :class="isUser ? 'rounded-s-xl rounded-ee-xl' : 'rounded-e-xl rounded-es-xl'">
             <div class="flex items-center space-x-2 rtl:space-x-reverse">
                 <span class="text-sm font-semibold text-gray-900 dark:text-white">{{ isUser ? 'User' : 'Analyzer'
                     }}</span>
