85a3ed014a3f7705567cc01e6b3e2e2ec5dcae97
ssabrut <michaeleco@Michaels-Laptop.local>
2024-03-02 12:45:56 +0700

    Change input to component

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,13 @@ import { Pinecone } from '@pinecone-database/pinecone';
 import { onMounted, ref } from 'vue';
 
 import Navigation from './components/Navigation.vue';
+import Input from './components/Input.vue';
 
 const pinecone = ref(null);
 const index = ref(null);
 
 onMounted(async () => {
-  pinecone.value = new Pinecone({apiKey: '7eb750c0-2b55-47f6-a3c0-cd7994703b21'});
+  pinecone.value = new Pinecone({ apiKey: '7eb750c0-2b55-47f6-a3c0-cd7994703b21' });
   index.value = pinecone.value.index('airline-sentiment-analysis');
 })
 </script>
@@ -18,19 +19,22 @@ onMounted(async () => {
     <Navigation />
   </nav>
   <main class="container mx-auto">
-    <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
-      <div class="px-4 py-2.5 bg-white rounded-t-lg">
-        <label for="comment" class="sr-only">Your comment</label>
-        <textarea id="comment" rows="4"
-          class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-0 focus:border-0 "
-          placeholder="Write a comment..." />
+    <div class="mb-8">
+      <div class="flex items-start gap-2.5">
+        <div
+          class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-blue-950 rounded-e-xl rounded-es-xl">
+          <div class="flex items-center space-x-2 rtl:space-x-reverse">
+            <span class="text-sm font-semibold text-gray-900 dark:text-white">Bonnie Green</span>
+            <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
+          </div>
+          <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">That's awesome. I think our users will
+            really appreciate the improvements.</p>
+          <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
+        </div>
       </div>
-      <div class="flex items-center justify-end px-3 py-2 border-t">
-        <button type="submit"
-          class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200">
-          Post comment
-        </button>
-      </div>
+    </div>
+    <div>
+      <Input />
     </div>
   </main>
 </template>
