429a57fab7a449b1c882c91ab55a1dee8e11ebbf
ssabrut <michael.gunawan2002@gmail.com>
2024-03-02 14:13:08 +0700

    Add postComment handler scaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { Pinecone } from '@pinecone-database/pinecone';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 import Navigation from './components/Navigation.vue';
 import Bubble from './components/Bubble.vue';
@@ -9,6 +9,7 @@ const pinecone = ref(null);
 const index = ref(null);
 const histories = ref([]);
 const comment = ref('');
+const errorState = ref(false);
 
 onMounted(async () => {
   pinecone.value = new Pinecone({ apiKey: import.meta.env.VITE_PINECONE_API_KEY });
@@ -17,11 +18,21 @@ onMounted(async () => {
     message: 'Hello, I am Airline Sentiment Bot. Start Writing Your Comments and I will analyze them for you.',
     isUser: false,
   });
-  histories.value.push({
-    message: 'Hello, I am Airline Sentiment Bot. Start Writing Your Comments and I will analyze them for you.',
-    isUser: true,
-  });
 })
+
+watch(comment, () => {
+  errorState.value = false;
+})
+
+const postComment = async () => {
+  if (comment.value.length === 0) {
+    errorState.value = true;
+    return;
+  }
+
+  histories.value.push({ message: comment.value, isUser: true });
+  comment.value = '';
+}
 </script>
 
 <template>
@@ -33,20 +44,25 @@ onMounted(async () => {
       <Bubble v-for="history in histories" :isUser="history.isUser" :message="history.message" />
     </div>
     <div>
-      <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50">
-        <div class="px-4 py-2.5 bg-white rounded-t-lg">
-          <label for="comment" class="sr-only">Your comment</label>
-          <textarea id="comment" rows="4"
-            class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-transparent focus:border-transparent" placeholder="Write your comment here..."
-            v-model="comment" />
-        </div>
-        <div class="flex items-center justify-end px-3 py-2.5 border-t">
-          <button type="submit"
-            class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-950 rounded-lg focus:ring-4 focus:ring-blue-200">
-            Post comment
-          </button>
-        </div>
+      <div class="w-full mb-1 border rounded-lg bg-gray-50" :class="errorState ? 'border-red-300' : 'border-gray-200'">
+        <form>
+          <div class="px-4 py-2.5 bg-white rounded-t-lg">
+            <label for="comment" class="sr-only">Your comment</label>
+            <textarea id="comment" rows="4"
+              class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-transparent focus:border-transparent"
+              placeholder="Write your comment here..." v-model="comment" />
+          </div>
+          <div class="flex items-center justify-end px-3 py-2.5 border-t">
+            <button type="button"
+              class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-950 rounded-lg focus:ring-4 focus:ring-blue-200"
+              :class="comment.length === 0 ? 'bg-gray-300' : 'bg-blue-950'" @click="postComment"
+              :disabled="comment.length == 0">
+              Post comment
+            </button>
+          </div>
+        </form>
       </div>
+      <span class="font-medium text-xs text-red-600" v-if="errorState">Comment Cannot be Empty!</span>
     </div>
   </main>
 </template>
