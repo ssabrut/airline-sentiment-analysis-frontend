5186bec6f1f355f3e4fac9fa0313b9cef6fe408b
ssabrut <michael.gunawan2002@gmail.com>
2024-03-03 21:06:29 +0700

    Finished

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,30 @@
 <script setup>
 import { Pinecone } from '@pinecone-database/pinecone';
 import { onMounted, ref, watch } from 'vue';
+import axios from 'axios';
+import { bouncy } from 'ldrs';
 
 import Navigation from './components/Navigation.vue';
 import Bubble from './components/Bubble.vue';
 
 const pinecone = ref(null);
 const index = ref(null);
+const baseUrl = ref('');
 const histories = ref([]);
 const comment = ref('');
 const errorState = ref(false);
+const isDisabled = ref(false);
 
 onMounted(async () => {
+  bouncy.register()
+  baseUrl.value = import.meta.env.VITE_BASE_URL;
   pinecone.value = new Pinecone({ apiKey: import.meta.env.VITE_PINECONE_API_KEY });
   index.value = pinecone.value.index(import.meta.env.VITE_PINECONE_INDEX);
+  baseUrl.value = import.meta.env.VITE_BASE_URL;
   histories.value.push({
-    message: 'Hello, I am Airline Sentiment Bot. Start Writing Your Comments and I will analyze them for you.',
+    message: 'Hello, I am Airline Sentiment Bot. You Could Write Your Comments or Review About an Airline and I will analyze them for you.',
     isUser: false,
+    isLoading: false,
   });
 })
 
@@ -30,8 +38,28 @@ const postComment = async () => {
     return;
   }
 
-  histories.value.push({ message: comment.value, isUser: true });
-  comment.value = '';
+  let index;
+  try {
+    isDisabled.value = true;
+    histories.value.push({ message: comment.value, isUser: true });
+    comment.value = '';
+
+    index = histories.value.push({ message: '', isUser: false, isLoading: true });
+    const response = await axios.post(`${baseUrl.value}/api/get-sentiment`, { text: comment.value }, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      }
+    });
+    if (response.data.status === 200) {
+      histories.value[index - 1].message = "Your Text Sentiment Are: " + response.data.data.sentiment;
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    histories.value[index - 1].isLoading = false;
+    isDisabled.value = false;
+  }
 }
 </script>
 
@@ -41,7 +69,8 @@ const postComment = async () => {
   </nav>
   <main class="container mx-auto">
     <div class="mb-8">
-      <Bubble v-for="history in histories" :isUser="history.isUser" :message="history.message" />
+      <Bubble v-for="history in histories" :isUser="history.isUser" :message="history.message"
+        :isLoading="history.isLoading" />
     </div>
     <div>
       <div class="w-full mb-1 border rounded-lg bg-gray-50" :class="errorState ? 'border-red-300' : 'border-gray-200'">
@@ -50,7 +79,7 @@ const postComment = async () => {
             <label for="comment" class="sr-only">Your comment</label>
             <textarea id="comment" rows="4"
               class="w-full px-0 text-sm text-gray-900 bg-white border-0 focus:ring-transparent focus:border-transparent"
-              placeholder="Write your comment here..." v-model="comment" />
+              placeholder="Write your comment here..." v-model="comment" :disabled="isDisabled" />
           </div>
           <div class="flex items-center justify-end px-3 py-2.5 border-t">
             <button type="button"
--- src/components/Bubble.vue
+++ src/components/Bubble.vue
@@ -8,17 +8,23 @@ defineProps({
         type: Boolean,
         required: true,
     },
+    isLoading: {
+        type: Boolean,
+        required: false,
+    },
 });
 </script>
 
 <template>
     <div class="flex items-start gap-2.5 mb-4" :class="isUser ? ' flex-row-reverse' : ''">
-        <img class="w-8 h-8 rounded-full" :src="isUser ? '/src/assets/user.png' :'/src/assets/pixlr-image-generator-51abc63a-2494-4e5f-8bf8-e3e8704617cd.png'"
+        <img class="w-8 h-8 rounded-full"
+            :src="isUser ? '/src/assets/user.png' : '/src/assets/pixlr-image-generator-51abc63a-2494-4e5f-8bf8-e3e8704617cd.png'"
             alt="Jese image">
-        <div
-            class="flex flex-col w-full max-w-4xl leading-1.5 p-4 border-gray-200 bg-blue-950" :class="isUser ? 'rounded-s-xl rounded-ee-xl' : 'rounded-e-xl rounded-es-xl'">
+        <l-bouncy size="45" speed="1.75" color="black" v-if="isLoading"></l-bouncy>
+        <div class="flex flex-col w-full max-w-2xl leading-1.5 p-4 border-gray-200"
+            :class="isUser ? 'rounded-s-xl rounded-ee-xl bg-blue-800' : 'rounded-e-xl rounded-es-xl bg-gray-600'" v-else>
             <div class="flex items-center space-x-2 rtl:space-x-reverse">
-                <span class="text-sm font-semibold text-gray-900 dark:text-white">{{ isUser ? 'User' : 'Analyzer'
+                <span class="text-sm font-semibold text-gray-900 dark:text-white">{{ isUser ? 'You' : 'Analyzer'
                     }}</span>
             </div>
             <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{{ message }}</p>
